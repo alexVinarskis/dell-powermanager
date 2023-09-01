a4eeee8bd997187e59f1f71499460cb792718ef2
Alex Vinarskis <alex.vinarskis@gmail.com>
2023-09-01 23:37:05 +0200

    chore: add github actions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build](https://github.com/alexVinarskis/dell-powermanager/actions/workflows/build.yml/badge.svg?branch=master)](https://github.com/alexVinarskis/dell-powermanager/actions/workflows/build.yml)
+
 # dell_powermanager
 
 Cross-Platform Dell Power Managment re-implementation
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dell_powermanager/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(title: 'title',));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
