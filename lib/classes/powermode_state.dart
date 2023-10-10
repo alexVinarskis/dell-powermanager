658e6bc5fb1acefa80923d596d8a9e79ad8de4b3
Alex <alex.vinarskis@gmail.com>
2023-10-10 17:18:25 +0200

    feat: windows power scheme info

--- lib/classes/powermode_state.dart
+++ lib/classes/powermode_state.dart
@@ -6,8 +6,15 @@ class PowermodeState {
   PowermodeState.fromLinuxResponse(String response) {
     profileInfo = _setIfPresent(response, (var x) => Powermode.profileInfoLinux.map.containsKey(x) ? Powermode.profileInfoLinux.map[x] : null);
   }
-  PowermodeState.fromWindowsResponse(Map<String, dynamic> map) {
-    // ToDo Windows integration;
+  PowermodeState.fromWindowsResponse(String response) {
+    if (response.isEmpty) {
+      return;
+    }
+    // power scheme's GUID is 4th element in the string
+    if (response.split(" ").length < 4) {
+      return;
+    }
+    profileInfo = _setIfPresent(response.split(" ")[3], (var x) => Powermode.profileInfoWindows.map.containsKey(x) ? Powermode.profileInfoWindows.map[x] : null);
   }
 
   _setIfPresent(var source, var transformation) {
