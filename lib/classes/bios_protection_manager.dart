e136a81497e143b1ffbd0b9f4a044fd6e556e067
Aleksandrs Vinarskis <alex.vinarskis@gmail.com>
2024-02-21 00:34:46 +0100

    feat: settings: option to delete BIOS password

--- lib/classes/bios_protection_manager.dart
+++ lib/classes/bios_protection_manager.dart
@@ -9,6 +9,16 @@ import '../configs/environment.dart';
 class BiosProtectionManager {
   static const storage = FlutterSecureStorage();
 
+  static final List<Function(String? biosPwd)> _callbacksBiosPwdChanged = [];
+  static void addCallbacksBiosPwdChanged(var callback)  { _callbacksBiosPwdChanged.add(callback); }
+  static void removeCallbacksBiosPwdChanged(var callback) { _callbacksBiosPwdChanged.remove(callback); }
+  static void _callBiosPwdChanged(String? biosPwd) {
+    var dubList = List.from(_callbacksBiosPwdChanged);
+    for (var callback in dubList) {
+      callback(biosPwd);
+    }
+  }
+
   static Future<void> secureReadPassword() async {
     String? pwdB64 = await storage.read(key: Constants.varnameBiosPwd);
     if (pwdB64 == null) {
@@ -29,8 +39,14 @@ class BiosProtectionManager {
     loadPassword(password);
   }
 
+  static Future<void> secureDeletePassword() async {
+    await storage.delete(key: Constants.varnameBiosPwd);
+    loadPassword("");
+  }
+
   static void loadPassword(String password) {
     Environment.biosPwd = password;
     ApiCCTK.sourceEnvironment();
+    _callBiosPwdChanged(Environment.biosPwd);
   }
 }
