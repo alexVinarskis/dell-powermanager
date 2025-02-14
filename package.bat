3c07d6c6f04598459e3a860454fced03e4ba97e6
Aleksandrs Vinarskis <alex@vinarskis.com>
2025-02-15 00:02:22 +0100

    feat: windows: build native arm64
    * Drop deprecated wmic in favor of PowerShell
    * Convert all non-cmd commands to cmd or PowerShell
    * Introduce arm64 native build support when running on arm64 host

--- package.bat
+++ package.bat
@@ -1,7 +1,7 @@
 
-: This script compiles Flutter app as windows application, and packages it to .msi
-: Root folder must contain Flutter application with `./build/windows`,
-:   for the rest, structure of application may change as required
+:: This script compiles Flutter app as windows application, and packages it to .msi
+:: Root folder must contain Flutter application with `.\build\windows`,
+::   for the rest, structure of application may change as required
 
 @echo off
 
@@ -15,42 +15,50 @@ for /f %%a in ('git describe HEAD --tags --abbrev^=0') do (
     set "VERSION_SHORT=%%a"
 )
 
-for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
-set Month=00%Month%
-set Day=00%Day%
-set Hour=00%Hour%
-set Minute=00%Minute%
-set Second=00%Second%
+for /f %%A in ('powershell -Command "(Get-Date).ToUniversalTime().ToString('yyyyMMdd-HHmmss')"') do set VER_DATE=%%A
 
-set "VER_DATE=%Year%%Month:~-2%%Day:~-2%-%Hour:~-2%%Minute:~-2%%Second:~-2%"
 set "VERSION_TAG=%VER_GIT%+%VER_DATE%"
 echo Version: %VERSION_TAG%
 echo Version short: %VERSION_SHORT%
 
-: Bake in app name and version tag to Flutter app
-sed -i "s|applicationName".*"|applicationName = '%NAME%';|g"                ./lib/configs/constants.dart
-sed -i "s|applicationVersion".*"|applicationVersion = '%VERSION_TAG%';|g"   ./lib/configs/constants.dart
+:: Detect system architecture
+:: Only 'master'channel of Flutter currently supports Windows arm64
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    set ARCH=x64
+    set ARCH_NAME=amd64
+) else if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+    set ARCH=arm64
+    set ARCH_NAME=arm64
+) else (
+    echo Unsupported architecture: %PROCESSOR_ARCHITECTURE%
+    exit /b 1
+)
 
-: Compile release app
+:: Bake in app name and version tag to Flutter app
+powershell -Command "(Get-Content ./lib/configs/constants.dart) -replace 'applicationName.*', 'applicationName = ''%NAME%'';' | Set-Content ./lib/configs/constants.dart"
+powershell -Command "(Get-Content ./lib/configs/constants.dart) -replace 'applicationVersion.*', 'applicationVersion = ''%VERSION_TAG%'';' | Set-Content ./lib/configs/constants.dart"
+
+:: Compile release app
 echo: && echo [1/2] Compiling Flutter app...
-call flutter build windows --release || exit 1
+call flutter build windows --release || exit /b 1
 
-: Package to .msi
+:: Package to .msi
 echo: && echo [2/2] Compiling .msi package...
 IF EXIST package rmdir /s /q package
 mkdir package
 
-cp resources/icon.ico package/
-cp resources/dell-powermanager.w* resources/dell-powermanager.sln package/
-cp -r build/windows/x64/runner/Release package/
+copy "resources\icon.ico" "package\"
+copy "resources\dell-powermanager.w*" "package\"
+copy "resources\dell-powermanager.sln" "package\"
+xcopy /E /I /Y "build\windows\%ARCH%\runner\Release" "package\Release"
 
-: Bake in app name and version to .msi package
-sed -i "s|1.0.0.0|%VERSION_SHORT%.0|g"                                      ./package/dell-powermanager.wxs
-sed -i "s|dell-powermanager|%NAME%|g"                                       ./package/dell-powermanager.wxs
+:: Bake in app name and version to .msi package
+powershell -Command "(Get-Content ./package/dell-powermanager.wxs) -replace '1.0.0.0', '%VERSION_SHORT%.0' | Set-Content ./package/dell-powermanager.wxs"
+powershell -Command "(Get-Content ./package/dell-powermanager.wxs) -replace 'dell-powermanager', '%NAME%' | Set-Content ./package/dell-powermanager.wxs"
 
-cd package && dotnet build dell-powermanager.wixproj /p:Platform=x64 /clp:ErrorsOnly --configuration Release || exit 1
+cd package && dotnet build dell-powermanager.wixproj /p:Platform=%ARCH% /clp:ErrorsOnly --configuration Release || exit /b 1
 cd ..
 
-cp package/bin/x64/Release/dell-powermanager.msi ./%PACKAGE%_%VERSION_TAG%_amd64.msi || exit 1
-echo Success! Produced './%PACKAGE%_%VERSION_TAG%_amd64.msi'
+copy "package\bin\%ARCH%\Release\dell-powermanager.msi" ".\%PACKAGE%_%VERSION_TAG%_%ARCH_NAME%.msi" || exit /b 1
+echo Success! Produced '.\%PACKAGE%_%VERSION_TAG%_%ARCH_NAME%.msi'
 rmdir /s/q package
